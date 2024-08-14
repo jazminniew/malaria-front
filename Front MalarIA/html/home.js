56723cd921517b699da9c38477a83f9213804138
jazminniew <jazminniew@gmail.com>
2024-08-13 23:39:54 -0400

    filtros añadidos falta mejorar

--- Front MalarIA/html/home.js
+++ Front MalarIA/html/home.js
@@ -5,17 +5,17 @@ const body = document.querySelector("body"),
     modeSwitch = body.querySelector(".toggle-switch"),
     modeText = body.querySelector(".mode-text");
 
-toggle.addEventListener("click", ()=>{
+toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
 });
 
-modeSwitch.addEventListener("click", ()=>{
+modeSwitch.addEventListener("click", () => {
     body.classList.toggle("dark");
 });
 
 // Filtros
 let products = {
-    data:[
+    data: [
         {
             patientName: "Marcelo",
             category: "Mas recientes",
@@ -57,10 +57,49 @@ for (let i of products.data) {
     document.getElementById("products").appendChild(patient);
     let container = document.createElement("div");
     container.classList.add("container");
-    //product name
+    // Nombre del paciente
     let name = document.createElement("h5");
     name.classList.add("product-name");
-    name.innerText = i.procutName.toUpperCase();
+    name.innerText = i.patientName.toUpperCase();
     container.appendChild(name);
-    
+    patient.appendChild(container);
 }
+
+// Filtro de categorías
+function filterProduct(value) {
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+        if (value.toUpperCase() === button.innerText.toUpperCase()) {
+            button.classList.add("active");
+        } else {
+            button.classList.remove("active");
+        }
+    });
+
+    let elements = document.querySelectorAll(".card");
+    elements.forEach((element) => {
+        if (value === "Todos") {
+            element.classList.remove("hide");
+        } else {
+            if (element.classList.contains(value)) {
+                element.classList.remove("hide");
+            } else {
+                element.classList.add("hide");
+            }
+        }
+    });
+}
+
+window.onload = () => {
+    filterProduct("Todos");
+};
+
+const searchInput = document.querySelector("#placeholder");
+
+searchInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        // Aquí puedes colocar la lógica de búsqueda
+        alert("Buscando: " + searchInput.value);
+        // Por ejemplo, podrías llamar a una función para filtrar los productos o hacer una búsqueda en el servidor
+    }
+});
