a229d0ce2b983194cc044ceb40caa7afac759e22
jazminniew <jazminniew@gmail.com>
2024-08-15 15:55:38 -0300

    mejora del sidebar

--- Front MalarIA/html/home2.js
+++ Front MalarIA/html/home2.js
@@ -1,8 +1,19 @@
 const cloud = document.getElementById("cloud");
 const barraLateral = document.querySelector(".barra-lateral"); 
-const span = document.querySelector("span");
+const spans = document.querySelectorAll("span");
+const palanca = document.querySelector(".switch");
+const circulo = document.querySelector(".circulo");
+
+palanca.addEventListener("click",()=>{
+    let body = document.body;
+    body.classList.toggle("dark-mode");
+    circulo.classList.toggle("prendido");
+})
 
 cloud.addEventListener("click", ()=>{ 
     barraLateral.classList.toggle("mini-barra-lateral");
-    span.classList.toggle("oculto");
+
+    spans.forEach((span)=>{
+        span.classList.toggle("oculto");
+    });
 });
