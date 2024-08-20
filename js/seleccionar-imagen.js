8fcdfd4fa23d8077fb7bc7d315163ca1975e748a
jazminniew <jazminniew@gmail.com>
2024-08-20 17:04:42 -0300

    upload pagina2

--- js/seleccionar-imagen.js
+++ js/seleccionar-imagen.js
@@ -1,5 +1,6 @@
 const selectImage = document.querySelector('.select-image');
 const inputFile = document.querySelector('#file');
+const imgArea = document.querySelector('.img-area');
 
 selectImage.addEventListener('click', function(){
     inputFile.click();
@@ -8,5 +9,12 @@ inputFile.addEventListener('change',function(){
     const image = this.files[0]
     console.log(image)
     const reader = new FileReader();
+    reader.onload = ()=>{
+        const imgUrl = reader.result;
+        const img = document.createElement('img');
+        img.src = imgUrl;
+        imgArea.appendChild(img);   
+    }
+    reader.readAsDataURL(image);
 }
 )
