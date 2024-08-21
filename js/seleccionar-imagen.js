6e879f93b1b3c03b01f1f4de2923a9724bcead69
jazminniew <jazminniew@gmail.com>
2024-08-20 21:49:04 -0400

    upload page "terminada"

--- js/seleccionar-imagen.js
+++ js/seleccionar-imagen.js
@@ -8,13 +8,21 @@ selectImage.addEventListener('click', function(){
 inputFile.addEventListener('change',function(){
     const image = this.files[0]
     console.log(image)
-    const reader = new FileReader();
-    reader.onload = ()=>{
-        const imgUrl = reader.result;
-        const img = document.createElement('img');
-        img.src = imgUrl;
-        imgArea.appendChild(img);   
-    }
-    reader.readAsDataURL(image);
+    if(image.size<2000000){
+        const reader = new FileReader();
+        reader.onload = ()=>{
+            const allImg = imgArea.querySelectorAll('img');
+            allImg.forEach(item=>item.remove());
+            const imgUrl = reader.result;
+            const img = document.createElement('img');
+            img.src = imgUrl;
+            imgArea.appendChild(img); 
+            imgArea.classList.add('active');  
+            imgArea.dataset.img = image.name;
+        }
+        reader.readAsDataURL(image);
 }
-)
+else{
+    alert("La imagen pesa mas de 2MB");
+}
+})
