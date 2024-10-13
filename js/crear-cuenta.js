70e60d402348177b20cfc1aae93044192c2d26cc
BatiOli9 <oliversafdieh@gmail.com>
2024-10-13 19:14:28 -0300

    fixes: Modifications in the user creation and login js for correct operation

--- js/crear-cuenta.js
+++ js/crear-cuenta.js
@@ -10,7 +10,7 @@ document.getElementById('registerForm').addEventListener('submit', async (event)
     };
 
     try {
-        const response = await fetch('https://malaria-xi.vercel.app/user/register', {
+        const response = await fetch('http://localhost:8000/user/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -28,37 +28,5 @@ document.getElementById('registerForm').addEventListener('submit', async (event)
         }
     } catch (error) {
         document.getElementById('response').innerText = `Error al registrar usuario: ${error.message}`;
-    }
-});
-document.getElementById('registerForm').addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    const formData = {
-        nombre: document.getElementById('nombre').value,
-        apellido: document.getElementById('apellido').value,
-        username: document.getElementById('username').value,
-        email: document.getElementById('email').value,
-        password: document.getElementById('password').value,
-    };
-
-    try {
-        const response = await fetch('https://malaria-xi.vercel.app/user/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        });
-
-        const result = await response.json();
-
-        if (response.ok) {
-            document.getElementById('response').innerText = 'Usuario registrado correctamente';
-        } else {
-            document.getElementById('response').innerText = `Error: ${result.message}`;
-        }
-    } catch (error) {
-        document.getElementById('response').innerText = `Error al registrar usuario: ${error.message}`;
-        response.classList.add('show'); // Mostrar el mensaje de error
     }
 });
--- js/iniciar-sesion.js
+++ js/iniciar-sesion.js
@@ -8,7 +8,7 @@ document.getElementById('registerForm').addEventListener('submit', async (event)
     };
 
     try {
-        const response = await fetch('https://malaria-xi.vercel.app/user/login', {
+        const response = await fetch('http://localhost:8000/user/login', { // Corrected typo here
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
